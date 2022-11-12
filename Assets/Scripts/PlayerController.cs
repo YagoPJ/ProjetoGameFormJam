7841e3d376ff0c18edb6e776242ccf652a33b356
Kaio Oliveira <105952427+YagoPJ@users.noreply.github.com>
2022-11-12 16:02:45 -0300

    Atualização
    Score OK

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,7 +27,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         lifeTxt.text = life.ToString();
         pontosTxt.text = pontos.ToString();
         //movimento
